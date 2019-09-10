cd9f0a3b1788a0d59eec3ab0a7a256a41b6c1cfe
22 aa <1023140570@qq.com>
2019-09-10 15:30:06 +0800

    完成函数has9(number)判断number中是否有9

--- WebApplication1/wwwroot/js/Homeword.0907.js
+++ WebApplication1/wwwroot/js/Homeword.0907.js
@@ -1,9 +1,9 @@
 ﻿//1.在函数student()中声明了函数域变量name、age和female，使用闭包机制，将其暴露到函数外部
-function student() {
-    var name
-    var age
-    var female
-}
+//function student() {
+//    var name
+//    var age
+//    var female
+//}
 
 //3.改动以下代码，让其输出如图所示，并说明理由。
 //function buildList(list) {
---
+++ WebApplication1/wwwroot/js/Homeword.0909.js
@@ -0,0 +1,17 @@
+﻿//1.构建一个函数has9(number) ，可以判断number中是否带有数字9；
+function has9(number) {
+    var parameter = String(number);
+    for (var i = 0; i < parameter.length; i++) {
+        if (parameter[i] === "9") {
+            return true;
+        } else {
+            if (i === parameter.length - 1) {
+                return false;
+            }
+        }
+    }
+}
+var has = has9()
+
+
+
