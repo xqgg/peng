2460ea7b09817b7fe19b45445bafa9a86b54c868
22 aa <1023140570@qq.com>
2019-09-10 20:36:40 +0800

    判断10000中有多少数包含"6"、"8"、"9"。

--- WebApplication1/wwwroot/js/Homeword.0909.js
+++ WebApplication1/wwwroot/js/Homeword.0909.js
@@ -1,17 +1,45 @@
 ﻿//1.构建一个函数has9(number) ，可以判断number中是否带有数字9；
-function has9(number) {
-    var parameter = String(number);
-    for (var i = 0; i < parameter.length; i++) {
-        if (parameter[i] === "9") {
-            return true;
-        } else {
-            if (i === parameter.length - 1) {
-                return false;
-            }
-        }
+//构建一个函数has8(number) ，可以判断number中是否带有数字8；
+//构建一个函数has6(number) ，可以判断number中是否带有数字6；
+
+/*测试：
+ * 输入hasX(9,999)=true;
+ * 输入hasX(9,119)=true;
+ * 输入hasX(9,-119)=请输入正整数;
+ * 输入hasX(5,-5.5)=请输入正整数;
+ * 输入hasX(5,NaN)=请输入正整数;
+ */
+
+function hasX(x, number) {
+    if (typeof (number) === 'number' && number > 0 && String(number)[1] != ".") {
+        var parameter = String(number);
+        var find = String(x);
+        return parameter.indexOf(find) >= 0;
+    } else {
+        console.log("请输入正整数");
     }
 }
-var has = has9()
 
+function has9(number) {
+    return hasX(9, number);
+}
+function has8(number) {
+    return hasX(8, number);
+}
+function has6(number) {
+    return hasX(6, number);
+}
 
+//2.使用上述函数，找出10000以内有多少个数字包含：9或者8或者6。
+/*测试：
+ */
 
+var result = 0;
+for (var i = 1; i < 10000 + 1; i++) {
+    if (has9(i) || has6(i) || has8(i)) {
+        result++;
+    } else {
+        //nothing
+    }
+}
+console.log("10000以内有" + result + "个数包含9")
