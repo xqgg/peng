2122c864bd49dfdaba3f2c91923fa6b7e79e4507
22 aa <1023140570@qq.com>
2019-09-14 16:56:35 +0800

    修复了学生人数可能等于0的BUG

--- WebApplication1/wwwroot/js/Homeword.0910.js
+++ WebApplication1/wwwroot/js/Homeword.0910.js
@@ -1,19 +1,12 @@
 ﻿//1.使用setTimeout() （不是setInterval() ）实现每隔1秒钟依次显示：第n周，源栈同学random人。
 //（n逐次递增，random随机）
-//测试结果为：每隔一秒显示：第n周，源栈同学random人。到第20周停止,random!>10。
-
-//setTimeout(function () {
-//    counter++;
-//    alert("第" + "counter" + "周，源栈同学" + "random" + "人");
-//    setTimeout(function () {
-//    });
-//});
+//测试结果为：每隔一秒显示：第n周，源栈同学random人。到第20周停止,random!>10，学生人数不等于0。
 var counter = 1
 function greet() {
     if (counter < 21) {
         setTimeout(function () {
             console.log("第" + counter + "周，源栈同学" +
-                Math.floor(Math.random() * (10 + 1)) + "人");
+                Math.ceil(Math.random() * 10) + "人");
             counter++
             greet();
         }, 1000)
@@ -22,3 +15,4 @@ function greet() {
     }
 }
 greet()
+Math.ceil
