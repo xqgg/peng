d8687ff20354385b1c1b004a4a4c2f14f924387e
22 aa <1023140570@qq.com>
2019-09-15 17:06:04 +0800

    优化了猜数字游戏

--- WebApplication1/wwwroot/js/Homeword.0910.js
+++ WebApplication1/wwwroot/js/Homeword.0910.js
@@ -23,6 +23,7 @@
 //浏览器生成一个不大于1000的随机正整数；
 //在猜测过程中点击取消直接结束游戏
 //用户输入猜测；
+//如果用户输入非规范的值，对用户做出相应提示
 //如果用户没有猜对，浏览器比较后告知结果：“大了”或者“小了”。如果用户：
 //只用了不到6次就猜到，弹出：碉堡了！
 //只用了不到8次就猜到，弹出：666！
@@ -30,50 +31,53 @@
 //用了10次都还没猜对，弹出：^ (*￣(oo) ￣)^
 
 var bingo = Math.ceil(Math.random() * 1000);
-if (confirm(`浏览器生成一个不大于1000的随机正整数；
-             用户输入猜测；
-             如果用户没有猜对，浏览器比较后告知结果：“大了”或者“小了”。如果用户：
-             只用了不到6次就猜到，弹出：碉堡了！
-             只用了不到8次就猜到，弹出：666！
-             用了8 - 10次猜到，弹出：猜到了。
-             用了10次都还没猜对，弹出：^ (*￣(oo) ￣)^`)) {
+if (confirm(
+    `浏览器生成一个不大于1000的随机正整数；
+    用户输入猜测；
+    如果用户没有猜对，浏览器比较后告知结果：“大了”或者“小了”。如果用户：
+    只用了不到6次就猜到，弹出：碉堡了！
+    只用了不到8次就猜到，弹出：666！
+    用了8 - 10次猜到，弹出：猜到了。
+    用了10次都还没猜对，弹出：^ (*￣(oo) ￣) ^`)) {
     bingoGame()
 } else {
     //取消
 }
 function bingoGame() {
-    for (var i = 0; i < 10; i++) {
-        var word = prompt("请输入您的猜测：", "");
-        if (!word) {
+    for (var i = 0; i < 100; i++) {
+        if (i >= 10) {
+            alert("^ (*￣(oo) ￣)^");
             break;
-            return false;
         }
-        if (!isNaN(word) && word > 0 && word.indexOf(".") == -1) {
-            if (+word == bingo) {
-                if (i < 6) {
-                    alert("碉堡了！");
-                } else {
-                    if (i < 8) {
-                        alert("666!");
-                    } else {
-                        alert("猜到了！");
-                    }
-                }
-                break;
-            } else {
-                if (i == 9) {
-                    alert("^ (*￣(oo) ￣)^");
-                    break;
-                }
-                if (+word > bingo) {
-                    alert("大了");
-                } else {
-                    alert("小了");
-                }
-            }
+        var word = prompt("请输入您的猜测：", "");
+        if (word === null) {
+            break;
+        }
+        if (isNaN(word)) {
+            alert("您输入的不是一个数字，请输入一个正整数");
+            continue;
+        }
+        if (word < 1) {
+            alert("您输入的不是一个正数，请输入一个正整数");
+            continue;
+        }
+        if (word.indexOf(".") > -1) {
+            alert("您输入的不是一个整数，请输入一个正整数");
+            continue;
+        }
+        if (+word > bingo) {
+            alert("大了");
+        } else if (+word < bingo) {
+            alert("小了");
+        } else if (i < 6) {
+            alert("碉堡了！");
+            break;
+        } else if (i < 8) {
+            alert("666！");
+            break;
         } else {
-            alert("请输入正整数");
+            alert("猜到了！");
+            break;
         }
     }
 }
-
