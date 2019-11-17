0d25190d0b41ef615af32d9ca0f6662b9f823b17
22 aa <1023140570@qq.com>
2019-11-17 21:20:32 +0800

    定义HelpMoney类

---
+++ ConsoleApp1/HolpMoney.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    class HolpMoney
+    {
+        string UserName;
+        int MoneyAmount;
+        void MoneyGain() { }
+        void MoneyExpense() { }
+
+
+    }
+}
