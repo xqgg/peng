ed7c5a8d718ea38c05c2902ca6b7c9f199ef1400
22 aa <1023140570@qq.com>
2019-11-17 21:13:42 +0800

    求助板块，定义一个Problem类

---
+++ ConsoleApp1/Problem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    public class Problem
+    {
+        string Title;
+        string Body;
+        int Reward;
+        DateTime PublishDateTime;
+        string Author;
+        void Publish() { }
+
+    }
+}
