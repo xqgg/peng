6f691c95afa3da8702082121adf12f62fc5fa2df
22 aa <1023140570@qq.com>
2019-11-22 13:25:42 +0800

    Respoitory用static

--- ConsoleApp1/Repoistory.cs
+++ ConsoleApp1/Repoistory.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CSharp
 {
-    class Repoistory
+    static class Repoistory
     {
         public const int version = 1;
         internal static string connection { get; }
