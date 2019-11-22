cd472d050a3bec8b157fd08b5aecc5acf28923f4
22 aa <1023140570@qq.com>
2019-11-22 13:24:38 +0800

    定义一个仓库（Repoistory）类，其中包含： 一个int类型的常量version 一个静态只读的字符串connection

---
+++ ConsoleApp1/Repoistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    class Repoistory
+    {
+        public const int version = 1;
+        internal static string connection { get; }
+
+
+    }
+}
