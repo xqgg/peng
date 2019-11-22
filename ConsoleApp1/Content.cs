c032744e13c0e894ef313ca5b39124f874b00f36
22 aa <1023140570@qq.com>
2019-11-22 13:45:17 +0800

    确保每个Content对象都有kind的非空值

---
+++ ConsoleApp1/Content.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    internal abstract class Content
+    {
+        protected string kind;
+        public Content(string kind)
+        {
+            if (kind == "")
+            {
+                Console.WriteLine("内容的种类不能为空，请重试");
+            }
+            else
+            {
+                this.kind = kind;
+            }
+        }
+    }
+}
