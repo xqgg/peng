feba9a8250bba25e23e593ee1c4665d28587a4cc
22 aa <1023140570@qq.com>
2019-11-26 11:07:20 +0800

    给链表添加IsHead /IsTail属性

---
+++ ConsoleApp1/DoubleLinked.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    public class DoubleLinked
+    {
+        public DoubleLinked Previous { get; private set; }
+        public DoubleLinked Next { get; private set; }
+        public bool IsHead
+        {
+            get
+            {
+                return Previous == null;
+            }
+        }
+        public bool IsTail
+        {
+            get
+            {
+                return Next == null;
+            }
+        }
+    }
+}
