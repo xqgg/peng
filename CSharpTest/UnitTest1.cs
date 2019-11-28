b39cc5306d17159cc8fe50afe6301ccba7fb92e5
22 aa <1023140570@qq.com>
2019-11-28 13:03:03 +0800

    完成InsertAfter测试用例

--- CSharpTest/UnitTest1.cs
+++ CSharpTest/UnitTest1.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-
+using CSharp;
 namespace CSharpTest
 {
     public class DoubleLinkedTest
@@ -12,8 +12,21 @@ namespace CSharpTest
         [Test]
         public void InsertAfter()
         {
-            Assert.Pass();
-            Assert.AreEqual(1 + 3, 4);
+            DoubleLinked old = new DoubleLinked();
+            DoubleLinked current_1 = new DoubleLinked();
+            current_1.InsertAfter(old);
+            Assert.AreEqual(current_1.Previous, old);
+            Assert.AreEqual(old.Next, current_1);
+            DoubleLinked current_2 = new DoubleLinked();
+            current_2.InsertAfter(old);
+
+            Assert.AreEqual(old.Next, current_2);
+            Assert.AreEqual(current_2.Next, current_1);
+
+            Assert.AreEqual(current_2.Previous, old);
+            Assert.AreEqual(current_1.Previous, current_2);
+
+
         }
 
         [Test]
