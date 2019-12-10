802c32a14722d5442727b89f299d6c629f2a2012
22 aa <1023140570@qq.com>
2019-12-10 16:19:21 +0800

    单元测试add方法

--- CSharpTest/TokenManagerTest.cs
+++ CSharpTest/TokenManagerTest.cs
@@ -9,7 +9,17 @@ namespace CSharpTest
         {
         }
 
-
+        [Test]
+        public void Add()
+        {
+            User pzq = new User();
+            pzq._tokens = new TokenManager();
+            pzq._tokens.Add(Token.Admin);
+            Assert.AreEqual((int)pzq._tokens._tokens, 2);
+            pzq._tokens.Add(Token.Blogger);
+            Assert.AreEqual((int)pzq._tokens._tokens, 6);
+          
+        }
 
 
 
