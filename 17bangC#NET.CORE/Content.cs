6ebfc5b823ac9f91e6c6ee7f2fdd6190dea6613a
22 aa <1023140570@qq.com>
2019-12-17 17:31:27 +0800

    Content的kind字段改用枚举

--- 17bangC#NET.CORE/Content.cs
+++ 17bangC#NET.CORE/Content.cs
@@ -4,33 +4,33 @@ using System.Text;
 
 namespace CSharp
 {
+    enum kind
+    {
+        Problem = 1,
+        Article = 2,
+        Suggest = 3
+    }
     internal abstract class Content
     {
+
         public string Title { get; set; }
         public string Body { get; set; }
         public User Author { get; set; }
         public DateTime PublishDateTime { get; set; }
         public void Publish()
-        { 
-        }
-        public Content(string kind)
         {
-            if (kind == string.Empty)
-            {
-                Console.WriteLine("内容的种类不能为空，请重试");
-                return;
-            }
-            else
-            {
-                this.kind = kind;
-            }
         }
-        protected string kind;
+        public Content(kind inputkind)
+        {
+            kind = inputkind;
+        }
+        protected kind kind;
         private DateTime _createTime;
         //private DateTime _PublishTime;
         public DateTime PublishTime
         {
             get { return _createTime; }
         }
+
     }
 }
