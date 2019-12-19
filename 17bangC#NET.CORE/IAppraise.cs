034fc7cdfb9978abced811ce830ba86fd25a8eac
22 aa <1023140570@qq.com>
2019-12-19 21:07:15 +0800

    新建IAppraise接口

---
+++ 17bangC#NET.CORE/IAppraise.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    interface IAppraise
+    {
+        public void Agree(User voter);
+        public void Disagree(User user);
+    }
+}
