f6b8add5117f5f69509a9c21de82cd7d099155c1
22 aa <1023140570@qq.com>
2020-01-02 16:16:01 +0800

    1.声明一个委托：打水（ProvideWater），可以接受一个Person类的参数，返回值为int类型

---
+++ Training1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training1
+{
+    //1.声明一个委托：打水（ProvideWater），可以接受一个Person类的参数，返回值为int类型
+    internal delegate int ProvideWater(Person person);
+
+    class Person
+    {
+        //public int GetWater(ProvideWater provideWater)
+        //{
+        //    provideWater(new Person());
+        //    return 1;
+        //}
+
+
+    }
+}
