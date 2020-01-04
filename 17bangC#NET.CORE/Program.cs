9c2902203b0c36d942dd3eddde457385c3673343
22 aa <1023140570@qq.com>
2020-01-04 11:30:48 +0800

    在Main()函数调用ContentService时，捕获一切异常，并记录（）异常的消息和堆栈信息

--- 17bangC#NET.CORE/Program.cs
+++ 17bangC#NET.CORE/Program.cs
@@ -15,9 +15,9 @@ namespace ConsoleApp1
 
             //ArticleTestDo.Do();
             //LinqQuery.LinqDo();
-            CSharp.TestGo.ProblemTestDo.Do();
+            //CSharp.TestGo.ProblemTestDo.Do();
             //CSharp.TestGo.ContentTestDo.Do();
-
+            CSharp.TestGo.ServiceTestDo.Do();
 
 
 
---
+++ 17bangC#NET.CORE/TestGo/ServiceTestDo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.TestGo
+{
+    class ServiceTestDo
+    {
+        public static void Do()
+        {
+            User user = new User("sd", "sf@@%4");
+            Article article = new Article(user, "sdsd");
+
+            //在Main()函数调用ContentService时，捕获一切异常，并记录（）异常的消息和堆栈信息
+            try
+            {
+                ContentService.Publish(article);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("记录异常的消息和堆栈信息");
+                //throw;
+            }
+        }
+    }
+}
