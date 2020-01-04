c19e6fc99f5d305e4384d090887c00a6fafa5c6e
22 aa <1023140570@qq.com>
2020-01-04 10:22:41 +0800

    将当前异常封装进新异常的InnerException，再将新异常抛出

--- 17bangC#NET.CORE/ContentService.cs
+++ 17bangC#NET.CORE/ContentService.cs
@@ -6,9 +6,18 @@ namespace CSharp
 {
     internal static class ContentService
     {
-        internal static void Publish( Content content)
+        internal static void Publish(Content content)
         {
-            content.Publish();
+            try
+            {
+                content.Publish();
+            }
+            //将当前异常封装进新异常的InnerException，再将新异常抛出
+            catch (ArgumentNullException ae)
+            {
+                Console.WriteLine("内容的作者不能为空");
+                throw new Exception("", new Exception("",ae));
+            }
             Console.WriteLine("将content存入数据库");
         }
     }
