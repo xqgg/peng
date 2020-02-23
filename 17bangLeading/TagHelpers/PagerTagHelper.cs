a41a6ef45324c465fd842bb45bdd577486ad16e4
22 aa <1023140570@qq.com>
2020-02-23 22:11:42 +0800

    自定义一个分页的PagerTagHelper，并将各个页面的后台命名空间改正为RazorPage

---
+++ 17bangLeading/TagHelpers/PagerTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPage.TagHelpers
+{
+    [HtmlTargetElement("pager", Attributes = "path,PageIndex")]
+    public class PagerTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+            output.TagName = "a";
+            object pageIndex = context.AllAttributes["pageIndex"].Value;
+            object path = context.AllAttributes["path"].Value;
+            output.Attributes.Add("href", $"{path}/page-{pageIndex}");
+            output.Attributes.Remove(context.AllAttributes["path"]);
+            output.Attributes.Remove(context.AllAttributes["pageIndex"]);
+        }
+    }
+}
