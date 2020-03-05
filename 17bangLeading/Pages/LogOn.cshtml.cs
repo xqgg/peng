36af8808b59985eb06eca7fa3294cdccc6a498b0
22 aa <1023140570@qq.com>
2020-03-05 14:54:07 +0800

    新建LogOn页面

---
+++ 17bangLeading/Pages/LogOn.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPage
+{
+    public class LogOnModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+
+        }
+    }
+}
