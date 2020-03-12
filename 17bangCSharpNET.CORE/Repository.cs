d181073f6e75efdcff8c0dd4219b52a02636e027
22 aa <1023140570@qq.com>
2020-03-12 20:53:26 +0800

    完成problem的动态页面以及分页功能

--- 17bangCSharpNET.CORE/Repository.cs
+++ 17bangCSharpNET.CORE/Repository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace CSharp
 {
@@ -15,19 +16,27 @@ namespace CSharp
 
     public class ProblemRepository
     {
-        public IList<Problem> Get()
+        static IList<Problem> problems;
+
+        static ProblemRepository()
         {
-            User yh1 = new User("用户1", "mima1!"),
-                 yh2 = new User("用户2", "mima2!");
+            User yh1 = new User("28zhu", "mima1!"),
+                 yh2 = new User("WhiteWater", "mima2!");
             Keyword CSharp = new Keyword("C#"),
                     JAVA = new Keyword("JAVA"),
+                    Windows = new Keyword("Windows"),
+                    Linux = new Keyword("Linux"),
                     CPlusPlus = new Keyword("C++");
-            List<Problem> problems = new List<Problem>
-            {
+            string title1 = "有一个自定义UI控件，此控件使用在不同的系统中会有不同的呈现，之前的做法是各种switch case，阅读代码时让人很难受，另外新创建一个用到此控件的系统，要修改代码的地方也多，只要有swich case 的地方都要再加一个case。请教一个好一些的方式来处理这个问题，目的是让代码更加清楚",
+                   title2 = "手动导入jar包，运行报错的问题";
+            string body1 = "RT，也不知道描述的清楚不清楚。求一个思路",
+                   body2 = "运行就报这个错误，这个jar包我导入项目了的，不然编译都无法通过。";
 
-                new Problem("内容1",yh1)
+            problems = new List<Problem>
+            {
+                new Problem(body1,yh1)
                 {
-                    Title = "标题1",
+                    Title=title1,
                     KeyWords = new List<Keyword>
                     {
                         CSharp,
@@ -35,34 +44,133 @@ namespace CSharp
                         JAVA
                     }
                  },
-                new Problem("内容2", yh1)
+                new Problem(body2, yh1)
                 {
-                    Title ="标题2",
-                    KeyWords=new List<Keyword>
-                    {
-                        CSharp
-                    }
-                },
-
-                 new Problem("内容3", yh2)
-                {
-                    Title ="标题3",
-                    KeyWords=new List<Keyword>
+                    Title = title2,
+                    KeyWords = new List<Keyword>
                     {
                         CSharp,
-                        CPlusPlus
+                        Linux
+                    }
+                },
+                new Problem(body2, yh2)
+                {
+                    Title = title2,
+                    KeyWords = new List<Keyword>
+                    {
+                        CSharp,
+                        CPlusPlus,
+                        Windows
+
+                    }
+                },
+                new Problem(body1,yh1)
+                {
+                    Title=title1,
+                    KeyWords = new List<Keyword>
+                    {
+                        CSharp,
+                        CPlusPlus,
+                        JAVA
+                    }
+                 },
+                new Problem(body2, yh2)
+                {
+                    Title = title2,
+                    KeyWords = new List<Keyword>
+                    {
+                        CSharp,
+                        CPlusPlus,
+                        Windows
+
+                    }
+                },
+                new Problem(body2, yh2)
+                {
+                    Title = title2,
+                    KeyWords = new List<Keyword>
+                    {
+                        CSharp,
+                        CPlusPlus,
+                        Windows
+
+                    }
+                },
+                new Problem(body1,yh1)
+                {
+                    Title=title1,
+                    KeyWords = new List<Keyword>
+                    {
+                        CSharp,
+                        CPlusPlus,
+                        JAVA
+                    }
+                 },
+                new Problem(body2, yh2)
+                {
+                    Title = title2,
+                    KeyWords = new List<Keyword>
+                    {
+                        CSharp,
+                        CPlusPlus,
+                        Windows
+
+                    }
+                },
+                new Problem(body1,yh1)
+                {
+                    Title=title1,
+                    KeyWords = new List<Keyword>
+                    {
+                        CSharp,
+                        CPlusPlus,
+                        JAVA
+                    }
+                 },
+                new Problem(body1,yh1)
+                {
+                    Title=title1,
+                    KeyWords = new List<Keyword>
+                    {
+                        CSharp,
+                        CPlusPlus,
+                        JAVA
+                    }
+                 },
+                new Problem(body2, yh2)
+                {
+                    Title = title2,
+                    KeyWords = new List<Keyword>
+                    {
+                        CSharp,
+                        CPlusPlus,
+                        Windows
 
                     }
                 }
-
             };
-
             foreach (var item in problems)
             {
                 ContentService.Publish(item);
             }
 
-            return problems;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="pageIndex">页码</param>
+        /// <param name="pageSize">数据量</param>
+        /// <returns></returns>
+        public List<Problem> Get(int pageIndex, int pageSize)
+        {
+            return problems.OrderByDescending(p => p.PublishTime)
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize).ToList();
+
+        }
+        public int CountOfProblem()
+        {
+            return problems.Count;
         }
     }
 }
--- 17bangLeading/Pages/Problem.cshtml.cs
+++ 17bangLeading/Pages/Problem.cshtml.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using CSharp;
 namespace RazorPage
 {
     public class ProblemModel : PageModel
     {
+        public List<Problem> problems { get; set; }
+        public int pageSize = 2;
+        public int pageAmount;
         public void OnGet()
         {
 
+            int pageIndex;
+            //未输入页码时默认显示第一页
+            if (Request.Query["page"].Count == 0)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                pageIndex = Convert.ToInt32(Request.Query["page"][0]);
+            }
+            ProblemRepository repository = new ProblemRepository();
+            problems = repository.Get(pageIndex, pageSize);
+            pageAmount = (repository.CountOfProblem() / pageSize) + 1;
         }
     }
 }
