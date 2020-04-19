89a7148f713d0f1e97a28826750bf2125cf7e038
22 aa <1023140570@qq.com>
2020-04-19 17:25:58 +0800

    删除share中_All中多余的分号，在LogOn页面中使用DualColumn完成前台渲染以及前台JS验证

--- 17bangLeading/Pages/LogOn.cshtml.cs
+++ 17bangLeading/Pages/LogOn.cshtml.cs
@@ -4,18 +4,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using CSharp;
 
 namespace RazorPage
 {
+    [BindProperties]
     public class LogOnModel : PageModel
     {
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "*用户名不能为空")]
+        [MaxLength(20, ErrorMessage = "*用户名长度不能大于20")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "*密码不能为空")]
+        [StringLength(20, ErrorMessage = "*密码长度不小于6不大于20", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "*验证码不能为空")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "*验证码长度不正确")]
+        public string SecurityCode { get; set; }
         public void OnGet()
         {
 
         }
         public void OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
 
+            if (correct(UserName, Password))
+            {
+                Response.Cookies.Append("UserId", "1");
+                return;
+            }
+
+            return;
+        }
+
+        /// <summary>
+        /// 检查用户输入的账号密码是否与数据库吻合。
+        /// </summary>
+        /// <param name="name">用户名</param>
+        /// <param name="password">密码</param>
+        /// <returns></returns>
+        private bool correct(string name, string password)
+        {
+            return true;
         }
     }
 }
